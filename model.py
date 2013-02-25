0fa1376dc4b246d75aa86e9d580d58fe91fde993
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-25 06:11:08 +0000

    encode properties and events in utf-8

--- model.py
+++ model.py
@@ -8,13 +8,15 @@ def items(profiles):
     for profile in profiles:
         uid = profile.uid
         for event in profile['events']:
+            event = event.encode('utf-8')
             yield event, uid
-            yield ' ', 'e:%s' % event.encode('utf-8')
+            yield ' ', 'e:%s' % event
         for prop_name, prop_values in profile['properties'].iteritems():
+            prop_name = prop_name.encode('utf-8') 
             yield ' ', 'p:%s' % prop_name.encode('utf-8')
             for prop_value in prop_values:
-                yield '%s:%s' % (prop_name[:MAX_LEN],
-                                 prop_value[:MAX_LEN]), uid
+                yield '%s:%s' % (prop_name, prop_value[:MAX_LEN].encode('utf-8')),\
+                      uid
                 
 @model_discodb
 def build(profiles):

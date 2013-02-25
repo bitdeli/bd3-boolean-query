ef08d3cba9b0f6880327807c413a36041f9eda8d
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-25 07:09:58 +0000

    fix storing of keys

--- model.py
+++ model.py
@@ -6,16 +6,18 @@ MAX_LEN = 64
 
 @model
 def items(profiles):
+    keys = set()
     for profile in profiles:
         uid = profile.uid
         for event in profile['events']:
             event = event.encode('utf-8')
             yield event, uid
-            yield ' ', 'e:%s' % event
+            keys.add(event)
         for prop_name, prop_values in profile['properties'].iteritems():
             prop_name = prop_name.encode('utf-8') 
-            yield ' ', 'p:%s' % prop_name
+            keys.add(prop_name)
             for prop_value in prop_values:
                 yield '%s:%s' % (prop_name, prop_value[:MAX_LEN].encode('utf-8')),\
                       uid
-                
+    for key in keys:
+        yield ' ', key

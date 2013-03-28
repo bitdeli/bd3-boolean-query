8c4be30c60138bf2d2fcc021589b59016685f782
Ville Tuulos <ville@bitdeli.com>
2013-03-28 05:11:51 +0000

    fix jsapi model

--- jsapi/model.py
+++ jsapi/model.py
@@ -1,24 +1,25 @@
 from discodb import DiscoDB
 from bitdeli.model import model
 
-
 MAX_LEN = 64
 
 @model
-def items(profiles):
+def build(profiles):
     keys = set()
     for profile in profiles:
         uid = profile.uid
         if not uid:
             continue
-        for event in profile['events']:
-            event = event.encode('utf-8')
-            yield event, uid
-            keys.add('e:' + event)
-        for prop_name, prop_values in profile['properties'].iteritems():
-            prop_name = prop_name.encode('utf-8') 
-            keys.add('p:' + prop_name)
-            for v in frozenset(prop_value[:MAX_LEN] for prop_value in prop_values):
-                yield '%s:%s' % (prop_name, v.encode('utf-8')), uid
+        fields = set()
+        for tstamp, group, ip, event in profile['events']:
+            e = 'e:%s' % event.pop('$event_name').encode('utf-8')
+            keys.add(e)
+            fields.add(e)
+            for prop_name, prop_value in event.iteritems():
+                p = 'p:%s' % prop_name.encode('utf-8')
+                keys.add(p)
+                fields.add('%s:%s' % (p, str(prop_value)[:MAX_LEN].encode('utf-8')))
+        for field in fields:
+            yield field, uid
     for key in keys:
-        yield ' ', key
+       yield ' ', key
